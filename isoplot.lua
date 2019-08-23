668266b8b31f681f4c6da8253407d41b21acbd91
Pulponite <pulponite@gmail.com>
2019-08-23 18:41:53 +0100

    Fix projection sizes

--- isoplot.lua
+++ isoplot.lua
@@ -79,6 +79,27 @@ function matrix:mult_vec(vec)
     return v
 end
 
+local function vec_len(v)
+    return math.sqrt((v[1] ^ 2) + (v[2] ^ 2) + ((v[3] or 0) ^ 2))
+end
+
+local function norlize_vec(v)
+    local length = vec_len(v)
+    return {
+        v[1] / length,
+        v[2] / length
+    }
+end
+
+local function vec_dot(v1, v2)
+    return (v1[1] * v2[1]) + (v1[2] * v2[2]) + ((v1[3] or 0) * (v2[3] or 0))
+end
+
+local function vec_intersect_plane(v, p, n)
+    local d = vec_dot(p, n) / vec_dot(v, n)
+    return {v[1] * d, v[2] * d, (v[3] or 0) * d}
+end
+
 local dlg = Dialog("IsoPlot")
 
 local function round(n)
@@ -94,7 +115,7 @@ local function get_direction(p1, p2)
 end
 
 local function attempt_fudge(s, r)
-    if r[1] < 0 then
+    if r[1] < 0 or (r[1] == 0 and r[2] > 0) then
         return translate(s, {-1, 0})
     end
     return s
@@ -102,6 +123,7 @@ end
 
 local function plot_line(p1, p2)
     local dir = get_direction(p1, p2)
+
     p1 = attempt_fudge(p1, dir)
     p2 = attempt_fudge(p2, dir)
 
@@ -145,33 +167,36 @@ local function center_quad(points)
 end
 
 local function make_shape()
-    -- Unprojection matrix
-    local um = matrix.identity()
-    um = um:mult_matrix(matrix.rotx(60))
-    um = um:mult_matrix(matrix.rotz(dlg.data.rotation))
-
-    local raxis = um:mult_vec({1, 0})
-    local rangle = math.atan(raxis[2] / raxis[1])
-    local xlen = dlg.data.right / math.cos(rangle)
-
-    local baxis = um:mult_vec({0, 1})
-    local bangle = math.abs(math.atan(baxis[2] / baxis[1]))
-    local ylen = (dlg.data.bottom - (xlen * math.sin(rangle))) / math.sin(bangle)
-
-    local p1 = {0, 0}
-    local p2 = {0, ylen}
-    local p3 = {xlen, ylen}
-    local p4 = {xlen, 0}
-
     local m = matrix.identity()
     m = m:mult_matrix(matrix.rotx(60))
     m = m:mult_matrix(matrix.rotz(dlg.data.rotation))
+
+    local raxis = m:mult_vec({1, 0})
+    local rangle = math.atan(raxis[2] / raxis[1])
+    local xlen = (dlg.data.right - 1) / math.cos(rangle)
+    local rpoint = {xlen * math.cos(rangle), xlen * math.sin(rangle), 0}
+    local rpoint2 = vec_intersect_plane(raxis, rpoint, {1, 0, 0})
+    local xlen2 = vec_len(rpoint2)
+
+    local baxis = m:mult_vec({0, 1})
+    local bangle = math.abs(math.atan(baxis[2] / baxis[1]))
+    local ylen = ((dlg.data.bottom - 1) - (xlen * math.sin(rangle))) / math.sin(bangle)
+    local bpoint = {ylen * -math.cos(bangle), ylen * -math.sin(bangle), 0}
+    local bpoint2 = vec_intersect_plane(baxis, bpoint, {0, 1, 0})
+    local ylen2 = vec_len(bpoint2)
+
+    local p1 = {0, 0}
+    local p2 = {0, ylen2}
+    local p3 = {xlen2, ylen2}
+    local p4 = {xlen2, 0}
+
     p1 = translate(m:mult_vec(p1), {0.5, 0.5})
     p2 = translate(m:mult_vec(p2), {0.5, 0.5})
     p3 = translate(m:mult_vec(p3), {0.5, 0.5})
     p4 = translate(m:mult_vec(p4), {0.5, 0.5})
 
     center_quad({p1, p2, p3, p4})
+
     app.transaction( function()
         app.command.NewLayer()
         plot_line(p1, p2)
